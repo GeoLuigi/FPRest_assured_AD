bb84b06b9ba052690e22b3a393e86e758fa80876
Jorge Hurtado <lhgjorge@gmail.com>
2023-12-04 18:01:10 -0400

    First jorge's trial

--- src/test/java/AccountModule.java
+++ src/test/java/AccountModule.java
@@ -1,2 +1,20 @@
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class AccountModule {
+
+    @Test(priority = 1)
+    void getUsers(){
+        given()
+        .when()
+            .get("https://reqres.in/api/users?page=2")
+        .then()
+            .statusCode(200)
+            .body("page", equalTo(2))
+            .log().all()
+        ;
+    }
 }
