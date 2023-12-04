d51bde78a3b3c833056465fb654672d68514cc66
Noelia <noelia.flores@training.assuresoft.com>
2023-12-04 18:23:44 -0400

    POST create user by Noe

--- src/test/java/AccountModule.java
+++ src/test/java/AccountModule.java
@@ -17,4 +17,20 @@ public class AccountModule {
             .log().all()
         ;
     }
+
+    @Test(priority = 1)
+    void createUser(){
+        JSONObject data = new JSONObject();
+        data.put("name", "morpheus");
+        data.put("job", "leader");
+
+        given()
+                .contentType("application/json")
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+        .then()
+               .statusCode(201)
+                .log().all();
+    }
 }
